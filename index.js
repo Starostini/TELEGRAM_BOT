d3fe229f0b3ec07bc63bf1799c389cb0c07ad4e4
Ilya Starostin <starostini@hotmail.com>
2022-06-23 00:07:54 +0300

    dotenv

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 const TelegramApi = require ('node-telegram-bot-api');
+require('dotenv').config(); // Вызов  dotenv
+const {gameOptions, againOptions} = require('./options.js')
 
-const token ='5556402948:AAFgaovRLjWoaarK9pBJc6mlvjkE-VvWorQ';
-
+const token = process.env.BOT_TOKEN;
+const url = process.env.APP_URL;
 const bot = new TelegramApi(token, {polling: true});
 
 const chats = {};
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: '1', callback_data: '1'},{ text: '2', callback_data: '2'},{ text: '3', callback_data: '3'}],
-      [{ text: '4', callback_data: '4'},{ text: '5', callback_data: '5'},{ text: '6', callback_data: '6'}],
-      [{ text: '7', callback_data: '7'},{ text: '8', callback_data: '8'},{ text: '9', callback_data: '9'}],
-      [{ text: '0', callback_data: '0'}]
-    ]
-  })
-}
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
 
-const againOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: 'Играть еще раз?', callback_data: '/again'}]
-    ]
-  })
-}
-
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
 const starGame = async (chatId) => {
   await bot.sendMessage(chatId, 'Сейчас я загадаю цифру от 0 до 9, а ты должен будешь ее отгадать!');
   const randomNumer = Math.floor(Math.random() *10);
